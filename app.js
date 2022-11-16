a3247dbc5405de5af6aeb9d967a40d107e90272b
Chae-G-0 <corkdud@naver.com>
2022-11-16 23:43:28 +0900

    221116

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const loginInput = document.querySelector("#loginForm input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
 function onLoginSubmit(e) {
   e.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
-  localStorage.setItem("userName", userName);
-  console.log(userName);
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+function paintGreetings(userName) {
   greeting.innerText = `Hello ${userName}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const saveUserName = localStorage.getItem(USERNAME_KEY);
+
+if (saveUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(saveUserName);
+}
