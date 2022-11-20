64f6efe49ee1f4fa83c32f36eaef535e9858d292
Chae-G-0 <corkdud@naver.com>
2022-11-20 23:41:56 +0900

    221120

---
+++ JS/todo.js
@@ -0,0 +1,28 @@
+const todoForm = document.getElementById("todoForm");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todoList");
+
+function deleteTodo(e) {
+    console.log(e)
+}
+
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
+}
+
+function handleTodoSubmit(e) {
+  e.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit);
